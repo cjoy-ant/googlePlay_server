6067f2b00b7c977a6dd3cb5652027f5153e521da
Christine Antonio <christinejm.antonio@gmail.com>
2021-03-03 11:30:33 -0600

    completed genres and sort filters

--- app.js
+++ app.js
@@ -1,53 +1,70 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
 app.use(morgan("common"));
 app.use(cors());
 
-const playstoreApps = require('./playstore.js')
+const playstoreApps = require("./playstore.js");
 
-app.get('/apps', (req, res) => {
-  const { sort, genres } = req.query
+app.get("/apps", (req, res) => {
+  const { sort, genres = "" } = req.query;
 
   if (genres) {
-    if (!["Action", "Puzzle", "Strategy", "Casual", "Arcade", "Card"].includes(genres)) {
-      return res.status(400).send("Genres must be one of the following: Action, Arcade, Card, Casual, Puzzle, Strategy")
+    if (
+      !["action", "puzzle", "strategy", "casual", "arcade", "card"].includes(
+        genres
+      )
+    ) {
+      return res
+        .status(400)
+        .send(
+          "Genres must be one of the following: Action, Arcade, Card, Casual, Puzzle, Strategy"
+        );
     }
   }
 
-  //if (genres) {
-  //  let results = playstoreApps.filter((item) => {
-  //    item["Genres"].includes(genres)
-  //  })
-  //  return results
-  //}
+  let results = playstoreApps.filter((item) =>
+    item["Genres"].toLowerCase().includes(genres.toLowerCase())
+  );
 
   if (sort) {
-    if (!["App", "Rating"].includes(sort)) {
-      return res.status(400).send("Sort must be one of app or rating")
+    if (!["app", "rating"].includes(sort)) {
+      return res.status(400).send("Sort must be one of app or rating");
     }
-  } 
-
-  let results = playstoreApps.filter((playstoreApp) => {
-    if (genres) {
-      playstoreApp["Genres"].includes(genres)
-    } else {
-      playstoreApp
-    }
-  })
-
-  if (sort) {
-    results.sort((a, b) => {
-      return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
-    })
   }
-  
-  res.json(playstoreApps)
-})
+
+  if (sort) {
+    let filter = "";
+
+    if (sort === "app") {
+      filter = "App";
+      results.sort((a, b) => {
+        return a[filter].toLowerCase() > b[filter].toLowerCase()
+          ? 1
+          : a[filter].toLowerCase() < b[filter].toLowerCase()
+          ? -1
+          : 0;
+      });
+    }
+
+    if (sort === "rating") {
+      filter = "Rating";
+      results.sort((a, b) => {
+        return a[filter] > b[filter] ? -1 : a[filter] < b[filter] ? 1 : 0;
+      });
+    }
+  }
+
+  if (genres) {
+    return res.send(results);
+  }
+
+  res.json(results);
+});
 
 app.listen(8000, () => {
-  console.log("Server started on PORT 8000 http://localhost:8000/app")
-})
+  console.log("Server started on PORT 8000 http://localhost:8000/apps");
+});
