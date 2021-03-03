6aeda426f08185bb4aad6a9091d789bf544766ad
Christine Antonio <christinejm.antonio@gmail.com>
2021-03-03 09:50:08 -0600

    sort, genres

--- app.js
+++ app.js
@@ -7,19 +7,45 @@ const app = express()
 app.use(morgan("common"));
 app.use(cors());
 
-const playstoreApps = require('./playstore')
+const playstoreApps = require('./playstore.js')
 
-app.get('/app', (req, res) => {
+app.get('/apps', (req, res) => {
   const { sort, genres } = req.query
 
-  if (sort) {
-
-  }
-
   if (genres) {
-
+    if (!["Action", "Puzzle", "Strategy", "Casual", "Arcade", "Card"].includes(genres)) {
+      return res.status(400).send("Genres must be one of the following: Action, Arcade, Card, Casual, Puzzle, Strategy")
+    }
   }
 
+  //if (genres) {
+  //  let results = playstoreApps.filter((item) => {
+  //    item["Genres"].includes(genres)
+  //  })
+  //  return results
+  //}
+
+  if (sort) {
+    if (!["App", "Rating"].includes(sort)) {
+      return res.status(400).send("Sort must be one of app or rating")
+    }
+  } 
+
+  let results = playstoreApps.filter((playstoreApp) => {
+    if (genres) {
+      playstoreApp["Genres"].includes(genres)
+    } else {
+      playstoreApp
+    }
+  })
+
+  if (sort) {
+    results.sort((a, b) => {
+      return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+    })
+  }
+  
+  res.json(playstoreApps)
 })
 
 app.listen(8000, () => {
